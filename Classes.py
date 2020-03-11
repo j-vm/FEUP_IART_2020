53920394217757f7dc6d423149eebb23f378df49
João Macedo <mac.j25@gmail.com>
2020-03-11 17:54:55 +0000

    update to the UI

---
+++ Classes.py
@@ -0,0 +1,32 @@
+class Photo:
+      
+       def __init__(self, id, info):
+              splitInfo = info.split()
+              self.id = id
+              self.Horizontal = True if splitInfo[0] == "H" else False
+              self.tags = set(splitInfo[2::])
+              print(self.tags)
+
+class Slide:
+       def __init__(self, photo):
+              self.photo1 = photo.id
+              self.photo2 = None
+              self.tags = photo.tags
+              self.points = 0
+              self.Horizontal = photo.Horizontal
+
+       def addVertical(self, photo):
+              if photo.Horizontal:
+                     print("Slide is Full")
+                     exit("Tried to add a photo to a slide that was full")
+              else:
+                     self.photo2 = photo.id
+                     self.tags.update(photo.tags)
+
+       def generateOutput(self):
+              if self.Horizontal:
+                     return str(self.photo1)
+              else:
+                     return str(self.photo1) + " " + str(self.photo2)
+
+
--- solve.py
+++ solve.py
@@ -1,11 +1,10 @@
 from tkinter import *
 from tkinter import ttk
 from tkinter.filedialog import askopenfilename
-from photo import Photo
+from Classes import Photo, Slide
 import score
 
-
-def calculate(*args):
+def loadFile(*args):
     f = open(filename, 'r')
 
     photos = []
@@ -15,8 +14,38 @@ def calculate(*args):
         if (lineNumber != 0):
             photos.append(Photo(lineNumber-1, line))
         lineNumber += 1
-    print(photos)
+    
+    fileLoaded()
     return photos
+
+
+
+def fileLoaded():
+    ttk.Label(mainframe, text="LOADED!").grid(column=1, row=8, sticky=(W, E))
+    ttk.Button(mainframe, text="Random Solution", command=solveRand).grid(column=1, row=9, sticky=W)
+    ttk.Button(mainframe, text="Greedy Algorithm", command=solveGreedy).grid(column=2, row=9, sticky=W)
+    ttk.Button(mainframe, text="Genetic Algorithm", command=solveGeneticAlgorithm).grid(column=3, row=9, sticky=W)
+    ttk.Button(mainframe, text="Simulated Anneling", command=solveSimulatedAnneling).grid(column=4, row=9, sticky=W)
+    ttk.Button(mainframe, text="Hill Climbing", command=solveHillClimbing).grid(column=5, row=9, sticky=W)
+
+
+
+
+def solveRand(*args):
+    return 1
+
+def solveGreedy(*args):
+    return 1
+
+def solveHillClimbing(*args):
+    return 1
+
+def solveGeneticAlgorithm(*args):
+    return 1
+
+def solveSimulatedAnneling(*args):
+    return 1
+
 
 
 root = Tk()
@@ -32,9 +61,7 @@ root.rowconfigure(0, weight=1)
 ttk.Label(mainframe, text="Input file path").grid(column=1, row=3, sticky=(W, E))
 ttk.Label(mainframe, text=filename).grid(column=1, row=4, sticky=(W, E))
 
-ttk.Button(mainframe, text="Greedy Algorithm", command=calculate).grid(column=4, row=3, sticky=W)
-ttk.Button(mainframe, text="Genetic Algorithm", command=calculate).grid(column=4, row=4, sticky=W)
-ttk.Button(mainframe, text="Simulated Anneling", command=calculate).grid(column=4, row=5, sticky=W)
+ttk.Button(mainframe, text="Load File", command=loadFile).grid(column=4, row=3, sticky=W)
 
 for child in mainframe.winfo_children(): child.grid_configure(padx=5, pady=5)
 
